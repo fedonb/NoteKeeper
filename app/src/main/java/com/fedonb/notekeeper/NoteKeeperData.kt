43cf3f589b84fc1460d1fccb4e87fa1d15e57a48
fedonbouzbas <fedon.bouzbas@bossinfo.ch>
2022-01-06 18:06:10 +0200

    Initial Commit

---
+++ app/src/main/java/com/fedonb/notekeeper/NoteKeeperData.kt
@@ -0,0 +1,9 @@
+package com.fedonb.notekeeper
+
+data class CourseInfo (val courseId: String, var title: String) {
+    override fun toString(): String {
+        return title
+    }
+}
+
+data class NoteInfo (var course: CourseInfo? = null, var title: String? = null, var text: String? = null)
---
+++ app/src/main/java/com/fedonb/notekeeper/NoteListActivity.kt
@@ -0,0 +1,44 @@
+package com.fedonb.notekeeper
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import com.fedonb.notekeeper.databinding.ActivityNoteListBinding
+import kotlinx.android.synthetic.main.content_note_list.*
+
+class NoteListActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityNoteListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityNoteListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        binding.fab.setOnClickListener { view ->
+            val activityIntent = Intent(this,MainActivity::class.java)
+            startActivity(activityIntent)
+        }
+
+        listNotes.adapter = ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                DataManager.notes)
+
+        listNotes.setOnItemClickListener{parent, view, position, id ->
+            val activityIntent = Intent(this, MainActivity::class.java)
+            activityIntent.putExtra(NOTE_POSITION, position)
+            startActivity(activityIntent)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (listNotes.adapter as ArrayAdapter<NoteInfo>).notifyDataSetChanged()
+    }
+}
